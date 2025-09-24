461faae0e3cf455753e294b386926461eaa869a2
coneastdev <coneastdev@proton.me>
2025-09-24 23:53:01 +0100

    Refine UX

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-print("Hello user, please input your search query, the more accurate the better result, this can be name, author and lyrics.")
+print("\nHello user, please input your search query, the more accurate the better result, this can be name, author and lyrics.")
 
 prompt = input("\nenter song query $ ").replace(" ", "%20")
 
@@ -11,7 +11,7 @@ response = requests.get(url, {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64
 soup = BeautifulSoup(response.text, "html.parser")
 songUrl = soup.find("article", class_="result").find("a", class_="url_header").get("href")
 
-f = open("output.txt", "w")
+print(f"\nUrl found. Loading content from {songUrl} ...")
 
 response = requests.get(songUrl, {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
 soup = BeautifulSoup(response.text, "html.parser")
@@ -21,6 +21,8 @@ soup.find("div", "LyricsHeader__Container-sc-5e4b7146-1").decompose()
 for br in soup.find_all('br'):
     br.replace_with("\n")
 
+f = open("output.txt", "w")
 f.write(soup.find("div", class_="Lyrics__Container-sc-a49d8432-1").get_text())
+f.close()
 
-print("lyrics outputted to \"output.txt\"")
+print("\nlyrics outputted to \"output.txt\"")
