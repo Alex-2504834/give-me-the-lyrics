1d779bd7cda6878514c5cb10a322d8a14a4169a5
coneastdev <coneastdev@proton.me>
2025-10-01 16:21:15 +0100

    Split into functions to prepare for unit tests.

--- app.py
+++ app.py
@@ -1,40 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 
-print("\nHello user, please input your search query, the more accurate the better result, this can be name, author and lyrics.")
+def getLyrics(prompt):
+    # create the url for get request and fire said request
+    url =  f"https://searx.bndkt.io/search?q=%22site%3Agenius.com%22%20{prompt}%20lyrics&language=all&time_range=&safesearch=0&categories=general"
+    response = requests.get(url, {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
 
-# get the search prompt and format it for the search engine
-prompt = input("\nenter song query $ ").replace(" ", "%20")
+    # process the raw html with soup so it can be easily modified and selected
+    soup = BeautifulSoup(response.text, "html.parser")
+    songUrl = soup.find("article", class_="result").find("a", class_="url_header").get("href")
 
-# create the url for get request and fire said request
-url =  f"https://searx.bndkt.io/search?q=%22site%3Agenius.com%22%20{prompt}%20lyrics&language=all&time_range=&safesearch=0&categories=general"
-response = requests.get(url, {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
+    print(f"\nUrl found. Loading content from {songUrl} ...")
 
-# process the raw html with soup so it can be easily modified and selected
-soup = BeautifulSoup(response.text, "html.parser")
-songUrl = soup.find("article", class_="result").find("a", class_="url_header").get("href")
+    # get html and process it with soup
+    response = requests.get(songUrl, {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
+    soup = BeautifulSoup(response.text, "html.parser")
 
-print(f"\nUrl found. Loading content from {songUrl} ...")
+    # remove header from lyrics
+    soup.find("div", {"data-exclude-from-selection": "true"}).decompose()
 
-# get html and process it with soup
-response = requests.get(songUrl, {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
-soup = BeautifulSoup(response.text, "html.parser")
+    # replace all the brs with new line characters so its more readable in the output
+    for br in soup.find_all('br'):
+        br.replace_with("\n")
 
-# remove header from lyrics
-soup.find("div", {"data-exclude-from-selection": "true"}).decompose()
+    # organise all lyrics into a string
+    output = ""
+    for verse in soup.find_all("div", {"data-lyrics-container": "true"}):
+        output += verse.get_text()
 
-# replace all the brs with new line characters so its more readable in the output
-for br in soup.find_all('br'):
-    br.replace_with("\n")
+    return output
 
-# organise all lyrics into a string
-output = ""
-for verse in soup.find_all("div", {"data-lyrics-container": "true"}):
-    output += verse.get_text()
 
-# output the lyrics to output.txt
-f = open("output.txt", "w")
-f.write(output)
-f.close()
+def main():
+    print("\nHello user, please input your search query, the more accurate the better result, this can be name, author and lyrics.")
 
-print("\nlyrics outputted to \"output.txt\"")
+    # get the search prompt and format it for the search engine
+    prompt = input("\nenter song query $ ").replace(" ", "%20")
+
+    output = getLyrics(prompt)
+
+    # output the lyrics to output.txt
+    f = open("output.txt", "w")
+    f.write(output)
+    f.close()
+
+    print("\nlyrics outputted to \"output.txt\" in active directory")
+
+main()
